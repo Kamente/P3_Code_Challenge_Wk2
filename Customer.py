732c32c0aeb5bb571a6aa50253c80ba6b8444b95
Kamente <justinm48000@gmail.com>
2023-08-27 00:20:29 +0200

    initialized the method instances for return

--- Customer.py
+++ Customer.py
@@ -1,5 +1,21 @@
 class Customer:
+    
+    customers = []
+    
     def __init__(self, given_name, family_name):
         self.given_name = given_name
         self.family_name = family_name
-        
+        self.customer.append(self) # to change after the customer is created
+    
+    def given_name(self): # instance method to return the given_name
+        return self.given_name
+    
+    def family_name(self):  # instance method to return the family_name
+        return self.family_name
+    
+    def full_name(self):  # instance method to return the full_name. i.e given_name and family_name
+        return f'{self.given_name} {self.family_name}'
+    
+    @classmethod # decorator method(return all the customer instances)
+    def all(cls):
+        return cls.customers
--- Restaurant.py
+++ Restaurant.py
@@ -1,4 +1,10 @@
 class Restaurant:
+    
+    restaurants = []
+    
     def __init__(self, name):
         self.name = name
+    
+    def name(self):
+        return self.name
     
