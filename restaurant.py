5327ee8217bd4f47bf89c78208f157671c056d74
Kamente <justinm48000@gmail.com>
2023-08-26 22:53:30 +0200

    delete

--- restaurant.py
+++
@@ -1,4 +0,0 @@
-class Restaurant:
-    def __init__(self, name):
-        self.name = name
-    
--- review.py
+++
@@ -1,5 +0,0 @@
-class Review:
-    def __init__(self, customer, restaurant,rating):
-        self.customer = customer
-        self.restaurant = restaurant
-        self.rating = rating
