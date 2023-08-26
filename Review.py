c193d07b2b4a64e0a6da19972de88a55b245be1c
Kamente <justinm48000@gmail.com>
2023-08-27 00:42:33 +0200

    method instances and decorator

--- Review.py
+++ Review.py
@@ -1,5 +1,13 @@
 class Review:
+    reviews = []
+    
     def __init__(self, customer, restaurant,rating):
         self.customer = customer
         self.restaurant = restaurant
         self.rating = rating
+    def rating(self):
+        return self.rating
+    
+    @classmethod
+    def all(cls):
+        return cls.reviews
