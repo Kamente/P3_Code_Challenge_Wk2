f41ac726380594d43634f7a6203972f77d7b71df
Kamente <justinm48000@gmail.com>
2023-08-27 19:24:16 +0300

    add review and return no of reviews

--- Customer.py
+++ Customer.py
@@ -5,6 +5,7 @@ class Customer:
         self.given_name = given_name
         self.family_name = family_name
         self.customer.append(self) # to change after the customer is created
+        self.reviews= []
     
     def given_name(self): # instance method to return the given_name
         return self.given_name
@@ -14,6 +15,13 @@ class Customer:
     
     def full_name(self):  # instance method to return the full_name. i.e given_name and family_name
         return f'{self.given_name} {self.family_name}'
+    
+    def add_review(self, restaurant, rating): # customer making new review
+        new_review = Review(self, restaurant, rating)
+        self.reviews.append(new_review)
+        
+    def num_reviews(self):
+        return len(self.reviews_list)
     
     @classmethod # decorator method(return all the customer instances)
     def all(cls):
