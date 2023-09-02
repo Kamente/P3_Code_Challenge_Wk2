1b172030786b568eb7703ba8b01a89be1a446495
Kamente <justinm48000@gmail.com>
2023-09-02 17:32:59 +0300

    customer

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from customer import Customer
 from review import Review
 from restaurant import Restaurant
 
-customer1 = Customer("Justin", "Kamente")
+customer1 = Customer("Kamente", "Justin")
 restaurant1 = Restaurant("Cratex")
 
 customer1.add_review(restaurant1, 4)
 average_rating = restaurant1.average_star_rating()
-print(f"Average rating for {restaurant1.name} = {average_rating}")
+print(f"Average rating for {restaurant1.name} by {customer1.full_name()} = {average_rating}")
